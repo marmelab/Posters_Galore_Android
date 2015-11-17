59d15decb0cef54a6c105776d32273dbc3ff9af8
Kmaschta <kevin@marmelab.com>
2015-11-17 10:39:47 +0100

    Improve unit testing

--- __tests__/components/ProductList.js
+++ __tests__/components/ProductList.js
@@ -5,15 +5,20 @@ import utils from 'react-addons-test-utils';
 import { ProductList } from '../../src/components/ProductList';
 
 describe('component ProductList', () => {
-    let props;
     let productList;
+    const defaultProps = {
+        products: [],
+        isFetching: false,
+        didInvalidate: false,
+        fetchProductsIfNeeded: sinon.spy(),
+    };
 
-    function renderProductList() {
+    function renderProductList(props) {
         const renderer = utils.createRenderer();
         renderer.render(<ProductList {...props} />);
         const output = renderer.getRenderOutput();
 
-        productList = {
+        return {
             props,
             output,
             renderer,
@@ -21,14 +26,7 @@ describe('component ProductList', () => {
     }
 
     beforeEach(() => {
-        props = {
-            products: [],
-            isFetching: false,
-            didInvalidate: false,
-            fetchProductsIfNeeded: sinon.spy(),
-        };
-
-        renderProductList();
+        productList = renderProductList(defaultProps);
     });
 
     it('should render correctly if no products', () => {
@@ -38,12 +36,14 @@ describe('component ProductList', () => {
     });
 
     it('should render products correctly', () => {
-        props.products = [{
-            thumbnail: 'some url',
-            price: 'THE PRICE',
-            stock: 'THE STOCK',
-        }];
-        renderProductList();
+        const props = Object.assign({}, defaultProps, {
+            products: [{
+                thumbnail: 'some url',
+                price: 'THE PRICE',
+                stock: 'THE STOCK',
+            }],
+        });
+        productList = renderProductList(props);
 
         const { output } = productList;
         assert.deepEqual(
@@ -56,9 +56,15 @@ describe('component ProductList', () => {
         assert.deepEqual(image.props.source.uri, 'some url');
 
         const [ price, stock ] = textView.props.children;
-        assert.notEqual(price.props.children.indexOf('THE PRICE'), -1,
-                        'The price is not correctly rendered');
-        assert.notEqual(stock.props.children.indexOf('THE STOCK'), -1,
-                        'The stock is not correctly rendered');
+        assert.notEqual(
+            price.props.children.indexOf('THE PRICE'),
+            -1,
+            'The price is not correctly rendered'
+        );
+        assert.notEqual(
+            stock.props.children.indexOf('THE STOCK'),
+            -1,
+            'The stock is not correctly rendered'
+        );
     });
 });
--- makefile
+++ makefile
@@ -8,5 +8,6 @@ run:
 	node node_modules/react-native/local-cli/cli.js run-android
 
 test:
+	rm -rf ./node_modules/jest-cli/.haste_cache
 	./node_modules/.bin/jest --verbose
 .PHONY: test
