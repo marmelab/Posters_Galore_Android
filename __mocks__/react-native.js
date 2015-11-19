af50268a7ca7be9b7c24b7d1758e460def62a7d8
Kmaschta <kevin@marmelab.com>
2015-11-19 12:46:09 +0100

    Remove deprecated import

--- __mocks__/react-native.js
+++ __mocks__/react-native.js
@@ -2,7 +2,7 @@
 /**
  * @see http://www.schibsted.pl/2015/10/testing-react-native-components-with-jest/
  */
-const React = require('react/addons');
+import React from 'react';
 const ReactNative = React;
 
 ReactNative.StyleSheet = {
